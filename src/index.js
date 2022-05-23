3e088fff09fed56359cb74ac6508010e0998d12f
Onlygirlintown <97001258+Onlygirlintown@users.noreply.github.com>
2022-05-23 16:47:05 -0700

    added react

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Dictionary from "./Dictionary";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Dictionary />
+  </React.StrictMode>
+);
