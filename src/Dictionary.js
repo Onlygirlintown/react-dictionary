1f4ce7a93e1b69ee1d7edf2f9421b46c85da414f
Onlygirlintown <97001258+Onlygirlintown@users.noreply.github.com>
2022-05-24 11:19:12 -0700

    api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`searching for ${keyword}`);
+
+    let word = `${keyword}`;
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function keyWordChange(event) {
