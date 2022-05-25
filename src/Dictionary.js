7e4ebfff78ea6006113507b1e8bc8a4b8330b79f
Onlygirlintown <97001258+Onlygirlintown@users.noreply.github.com>
2022-05-25 15:46:28 -0700

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -30,7 +30,7 @@ export default function Dictionary() {
         <input type="search" autoFocus={true} onChange={keyWordChange} />
         <input type="submit" />
       </form>
-      <h2 className="text=capitalize">{keyword}!!!</h2>
+      <h2 className="text=capitalize">{keyword}</h2>
       <Results results={results} />
     </div>
   );
