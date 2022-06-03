ef375cb77e1dc7dc5dd57114e342f945cfc7d082
Onlygirlintown <97001258+Onlygirlintown@users.noreply.github.com>
2022-06-03 15:40:40 -0700

    added photos component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState("");
+  let [photos, setPhotos] = useState("");
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    console.log(response);
+    setPhotos(response.data.photos);
   }
 
   function search(event) {
@@ -17,7 +24,14 @@ export default function Dictionary() {
     let word = `${keyword}`;
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
 
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelApiKey =
+      "563492ad6f917000010000018d00173e8d184b77be4a38cdfd58647f";
+    let pexelApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    axios
+      .get(pexelApiUrl, { headers: { Authorization: `Bearer ${pexelApiKey}` } })
+      .then(handlePexelsResponse);
   }
 
   function keyWordChange(event) {
@@ -30,6 +44,7 @@ export default function Dictionary() {
         <input type="search" autoFocus={true} onChange={keyWordChange} />
         <input type="submit" />
       </form>
+      <Photos photos={photos} />
       <Results results={results} />
     </div>
   );
---
+++ src/Photos.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function Photos(props) {
+  console.log(props);
+
+  if (props.photos) {
+    return (
+      <div className="row">
+        {props.photos.map(function (photos, index) {
+          return (
+            <div className="col-4" key={index}>
+              <a href={photos.src.original} target="_blank" rel="noreferrer">
+                <img
+                  src={photos.src.landscape}
+                  className="img-fluid"
+                  alt={photos.alt}
+                />
+              </a>
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
