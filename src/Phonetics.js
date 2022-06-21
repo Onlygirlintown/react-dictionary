7e8d24d9d97a013589dc7d33555d4e490cba4daa
Onlygirlintown <97001258+Onlygirlintown@users.noreply.github.com>
2022-06-21 12:16:41 -0700

    added str length to phonetics component

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -3,13 +3,39 @@ import "./Phonetics.css";
 
 export default function Phonetics(props) {
   console.log(props.phonetics);
-  return (
-    <div className="phoneticsDiv">
-      <a href={props.phonetics[0].audio} target="_blank" rel="noreferrer">
-        listen 🗣️
-      </a>
-      <br />
-      {props.phonetics[0].text}
-    </div>
-  );
+  const audioLink = props.phonetics[0].audio;
+  const audioLinkTwo = props.phonetics[1].audio;
+
+  if (audioLink.length > 0) {
+    return (
+      <div className="phoneticsDiv">
+        <a href={props.phonetics[0].audio} target="_blank" rel="noreferrer">
+          listen 🗣️
+        </a>
+        <br />
+        {props.phonetics[0].text}
+      </div>
+    );
+  }
+  if (audioLinkTwo.length > 0) {
+    return (
+      <div className="phoneticsDiv">
+        <a href={props.phonetics[1].audio} target="_blank" rel="noreferrer">
+          listen 🗣️
+        </a>
+        <br />
+        {props.phonetics[0].text}
+      </div>
+    );
+  } else {
+    return (
+      <div className="phoneticsDiv">
+        <a href={props.phonetics[2].audio} target="_blank" rel="noreferrer">
+          listen 🗣️
+        </a>
+        <br />
+        {props.phonetics[0].text}
+      </div>
+    );
+  }
 }
